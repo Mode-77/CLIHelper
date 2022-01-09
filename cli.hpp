ce5f160419fd74a3399b6f4ec910c8ad35e26099
Jared Thomas <sci.imagino@gmail.com>
2022-01-09 14:57:24 -0500

    Rearranged the function declarations

--- cli.hpp
+++ cli.hpp
@@ -23,29 +23,33 @@
 void readInput(std::string &buffer, std::istream &stream = std::cin);
 
 
-//Returns the number of fields in the line
-unsigned int fieldCount(const std::string &input);
+//Returns true if there are no fields in the line or false
+//if the line has at least one field.
+bool allFieldsEmpty(const std::string &line);
 
 
-//Returns the number of arguments in the line.
-//
-//Arguments are any fields after the first.
-//
-//line must have at least one field.
-unsigned int argumentCount(const std::string &line);
-
-
-//Returns true if there are no fields in the line,
-//returns false otherwise
-bool allFieldsEmpty(const std::string &fields);
+//Returns the number of fields in the line.
+unsigned int fieldCount(const std::string &line);
 
 
 //Returns true if the field at index matches the key,
 //returns false otherwise
 //
-//    fieldCount(input) > 0
-//    0 <= index < fieldCount(input)
-bool fieldIs(const char *key, const size_t index, const char *input);
+//    fieldCount(line) > 0
+//    0 <= index < fieldCount(line)
+bool fieldIs(const char *key, const size_t index, const char *line);
+
+
+//Stores the field at index in result.
+//
+//Original contents of result are lost.
+//
+//    fieldCount(line) > 0
+//    0 <= index < fieldCount(line)
+void extractField(
+    const std::string &line,
+    const size_t index,
+    std::string &result);
 
 
 //Returns true if the head field matches the key or false
@@ -57,31 +61,27 @@ bool fieldIs(const char *key, const size_t index, const char *input);
 bool headIs(const std::string &key, const std::string &line);
 
 
+//Returns true if line has no arguments or false if it has
+//at least one argument.
+//
+//line must have at least the head field.
+bool noArguments(const std::string &line);
+
+
+//Returns the number of arguments in the line.
+//
+//Arguments are any fields after the first.
+//
+//line must have at least one field.
+unsigned int argumentCount(const std::string &line);
+
+
 //Returns true if the Nth argument matches key or false
 //if it does not.
 //
 //line must have at least one argument.
 //N must be in the range [0, argumentCount(line) - 1].
 bool argumentIs(const std::string &key, size_t N, const std::string &line);
-
-
-//Stores the field at index in result.
-//
-//Original contents of result are lost.
-//
-//    fieldCount(input) > 0
-//    0 <= index < fieldCount(input)
-void extractField(
-    const std::string &input,
-    const size_t index,
-    std::string &result);
-
-
-//Returns true if line has no arguments or false if it has
-//at least one argument.
-//
-//line must have at least the head field.
-bool noArguments(const std::string &line);
 
 
 //Returns the Nth argument in the line.
